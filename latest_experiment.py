6acdb60cd8bbcd9c538a754377e0e81c1ffe47da
luise <luise.haeuser@web.de>
2025-02-05 11:55:37 +0100

    Refactor and tidy up

---
+++ latest_experiment.py
@@ -0,0 +1,58 @@
+import os
+from tabulate import tabulate
+from Bio import AlignIO
+import pandas as pd
+
+def get_difficulty(msa_name):
+    prefix = os.path.join("data/pythia", msa_name + ".pythia")
+    if not os.path.isfile(prefix):
+        return float("nan")
+    with open(prefix, "r") as outfile:
+        lines = outfile.readlines()
+        if len(lines) == 0:
+            return float("nan")
+        return float(lines[0])
+
+def run_pythia(msa_name, redo):
+    msa_path = os.path.join("data/msa", msa_name + ".phy")
+    if not os.path.isfile(msa_path):
+        print("MSA " + msa_name + " does not exist")
+        return
+    AlignIO.read(msa_path, "phylip-relaxed")
+    predictor_path = os.path.join("predictors/latest.pckl")
+    prefix = os.path.join("data/pythia", msa_name + ".pythia")
+    if os.path.isfile(prefix) and not redo:
+        print("Files with prefix " + prefix + " already exist")
+        return
+    prefix_dir = os.path.dirname(prefix)
+    if not os.path.isdir(prefix_dir):
+        os.makedirs(prefix_dir)
+    command = "pythia -m " + msa_path + " -o " + prefix + " -r bin/raxml-ng -p " + predictor_path + " --removeDuplicates -v"
+    print(command)
+    os.system(command)
+
+
+msa_names = [name.split(".")[0] for name in os.listdir("data/msa")]
+ground_truths = {msa_name: 0 for msa_name in msa_names}
+df = pd.read_parquet('all_data.parquet')
+absolute_errors = []
+for i, row in df.iterrows():
+    ground_truths[row["verbose_name"].split(".")[0]] = row["difficult"]
+res = []
+for msa_name in msa_names:
+    row = [msa_name, ground_truths[msa_name]]
+    #run_pythia(msa_name, redo = True)
+    d = get_difficulty(msa_name)
+    row.append(d)
+    absolute_error = abs(ground_truths[msa_name]-d)
+    row.append(absolute_error)
+    absolute_errors.append(absolute_error)
+    res.append(row)
+headers = ["MSA", "ground truth", "prediction", "error"]
+#print(tabulate(res, tablefmt="pipe", floatfmt=".2f", headers = headers))
+
+mae = sum(absolute_errors) / len(absolute_errors)
+print(f"""
+Overall Performance with Latest Predictor:
+- Mean Absolute Error: {round(mae, 2)}
+""")
---
+++ loo_experiment.py
@@ -0,0 +1,82 @@
+from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error
+import math
+import pandas as pd
+import lightgbm as lgb
+
+# NOTE: adjust the path to point to your language features and remove the .sample part (keep the reset_index to make sure the LOO works as expected)
+# tdf = pd.read_parquet("training_data_pythia_v1.1.0.parquet").sample(45, random_state=0).reset_index(drop=True)
+
+
+def train_loo(tdf, features, label):
+    """
+    Leave-one-out cross-validation
+    """
+    predictions = []
+    prediction_mae = []
+    prediction_mape = []
+    training_mae = []
+    training_mape = []
+
+    for i in range(tdf.shape[0]):
+        # Train a regressor using all FEATURES and the LABEL as target using all data except the i-th row
+        train_df = tdf.drop(i)
+        test_df = tdf.iloc[[i]]
+
+        params = {
+            "max_depth": 5,
+            "num_leaves": 30
+        }
+
+        model = lgb.LGBMRegressor(**params, verbose=-1)
+        model.fit(train_df[features], train_df[label])
+
+        # Predict the i-th row
+        prediction = model.predict(test_df[features])[0]
+        predictions.append(prediction)
+
+        mae = mean_absolute_error(test_df[label], [prediction])
+        mape = mean_absolute_percentage_error(test_df[label], [prediction])
+        prediction_mae.append(mae)
+        prediction_mape.append(mape)
+
+        # Evaluate the model on the training data
+        train_predictions = model.predict(train_df[features])
+        mae = mean_absolute_error(train_df[label], train_predictions)
+        mape = mean_absolute_percentage_error(train_df[label], train_predictions)
+        training_mae.append(mae)
+        training_mape.append(mape)
+
+    return predictions, prediction_mae, prediction_mape, training_mae, training_mape
+
+
+tdf = pd.read_parquet("all_data.parquet").reset_index(drop=True)
+tdf = tdf.rename(columns={"num_topos_parsimony/num_trees_parsimony": "proportion_unique_topos_parsimony"})
+tdf["num_patterns/num_sites"] = tdf["num_patterns"] / tdf["num_sites"]
+tdf["pattern_entropy"] = tdf["bollback"] + tdf["num_sites"] * tdf["num_sites"].apply(lambda x: math.log(x))
+
+# ordered by Pythia importance from most to least important
+FEATURES = [
+    'avg_rfdist_parsimony',
+    'num_patterns/num_taxa',
+    'bollback',
+    'proportion_gaps',
+    'proportion_invariant',
+    'entropy',
+    "num_patterns/num_sites",
+    "pattern_entropy",
+    'num_sites/num_taxa',
+    "proportion_unique_topos_parsimony",
+]
+
+LABEL = "difficult"
+
+predictions, prediction_mae, prediction_mape, training_mae, training_mape = train_loo(tdf, FEATURES, LABEL)
+mae = mean_absolute_error(tdf[LABEL], predictions)
+mape = mean_absolute_percentage_error(tdf[LABEL], predictions)
+
+
+print(f"""
+Overall LOO Performance:
+- Mean Absolute Error: {round(mae, 2)}
+- Mean Absolute Percentage Error: {round(mape, 2)}%
+""")
